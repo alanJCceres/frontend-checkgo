e32e65a3d5d6d218b5ed1ae95462d1e76a7ad03b
Alan Cáceres <caceresalan2474@gmail.com>
2026-09-21 23:15:47 -0400

    mejora en pantalla login al cerrar sesion

--- app/src/main/java/com/example/checkgo/core/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/checkgo/core/navigation/AppNavigation.kt
@@ -1,10 +1,13 @@
 package com.example.checkgo.core.navigation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,6 +26,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.checkgo.core.ui.components.AppBottomBar
 import com.example.checkgo.feature_admin.presentation.screens.UsersScreen
@@ -39,6 +43,7 @@ fun AppNavigation(
 // Obtenemos la ruta actual para saber dónde está el usuario
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+    val isImmersiveScreen = currentRoute == "login" //verificamos si la ruta es login
     if (isLoading) {
         Box(
             modifier = Modifier.fillMaxSize(),
@@ -48,6 +53,14 @@ fun AppNavigation(
         }
     } else {
         Scaffold(
+            //configuracion para hacer completa la pantalla login
+            contentWindowInsets = if (isImmersiveScreen) {
+                WindowInsets(0.dp, 0.dp, 0.dp, 0.dp)
+            } else {
+                ScaffoldDefaults.contentWindowInsets
+            },
+            //color del container para hacer el login pantalla completa
+            containerColor = if (isImmersiveScreen) Color.Transparent else MaterialTheme.colorScheme.background,
             bottomBar = {
                 when {
                     // Si la ruta actual pertenece al flujo Admin
